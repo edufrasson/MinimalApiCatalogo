a009482aca63c20d0162bca0307ce7e10e76f849
Eduardo Frasson <edudomine2016@gmail.com>
2023-08-23 21:33:34 -0300

    feat: criando endpoints de categoria

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,4 +1,6 @@
-﻿namespace ApiCatalogo.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiCatalogo.Models
 {
     public class Categoria
     {
@@ -6,6 +8,8 @@
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
 
+        
+        [JsonIgnore] // Não exibir essa propriedade no Post, pois este é um atributo de navegação.
         public ICollection<Produto>? Lista_Produtos { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ApiCatalogo.Context;
+using ApiCatalogo.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,57 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
+
+/* Definição dos EndPoints */
+app.MapGet("/", () => "Catálogo de Produtos - 2023");
+
+// Tabela Categoria
+
+app.MapPost("/categorias", async (Categoria c, AppDbContext db) =>
+{
+    db.Categorias.Add(c);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/categorias/{c.Id}", c);
+});
+
+app.MapGet("/categorias", async(AppDbContext db) => await db.Categorias.ToListAsync());
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categorias.FindAsync(id)
+        is Categoria c ? Results.Ok(c) : Results.NotFound();
+});
+
+app.MapPut("/categorias/{id:int}", async (int id, Categoria c, AppDbContext db) =>
+{
+    if(id != c.Id)
+    {
+        return Results.BadRequest();
+    }
+
+    var categoriaDB = await db.Categorias.FindAsync(id);
+
+    if (categoriaDB is null) return Results.NotFound();
+
+    categoriaDB.Nome = c.Nome;
+    categoriaDB.Descricao = c.Descricao;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(categoriaDB);
+});
+
+app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db) =>
+{
+    var categoria = await db.Categorias.FindAsync(id);
+    if (categoria is null)
+        return Results.NotFound();
+
+    db.Categorias.Remove(categoria);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
